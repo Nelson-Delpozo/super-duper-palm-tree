485748fedc2f249d20e513016b4df1d7d6ac9298
Nelson Delpozo <nelson.delpozo99@gmail.com>
2022-05-30 21:34:46 -0600

    started

---
+++ src/main/java/Controllers/HelloController.java
@@ -0,0 +1,21 @@
+package Controllers;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+class HelloController {
+
+    @GetMapping("/")
+    @ResponseBody
+    public String hello() {
+        return "Hello from Spring!";
+    }
+
+
+
+
+}
